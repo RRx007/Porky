1ff58dfadc9420c21edec4c33a01a06b084632eb
Riicky <Riicky@DESKTOP-5U9BOG6>
2025-02-16 04:21:28 -0500

    Adding porky tcp scan

---
+++ Classes/Porky.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porky.Classes
+{
+    public class Porky
+    {
+        public Porky() {}
+
+        public  async Task Sizzle(string target, int startPort, int endPort)
+        {
+            List<int> openPorts = new List<int>();
+            List<Task> tasks = new List<Task>();
+
+            Console.WriteLine($"Scanning {target} for open ports between {startPort}-{endPort}..");
+            for (int port = startPort; port < endPort; port++) 
+            {
+                tasks.Add(ScanTcpAsync(target, port, openPorts));
+            }
+
+            await Task.WhenAll(tasks);
+
+            Console.WriteLine("\nScan complete! Open ports: ");
+
+            foreach (var port in openPorts) 
+            {
+                Console.WriteLine($"Port {port} - .....");
+            }
+        }
+
+        public async Task ScanTcpAsync(string host, int port, List<int> openPorts) 
+        {
+            using (TcpClient client = new TcpClient())
+            {
+                try
+                {
+                    var connectTask = client.ConnectAsync(host, port);
+                    if (await Task.WhenAny(connectTask, Task.Delay(500)) == connectTask)
+                    {
+                        Console.WriteLine($"[+] Open (TCP): {port}");
+                        openPorts.Add(port);
+                        await GrabbingBannerTcpAsync(client, port);
+                    }
+                }
+                catch(Exception) { }
+            }
+        }
+
+        public async Task GrabbingBannerTcpAsync(TcpClient client, int port)
+        {
+            try 
+            {
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+                int bytesRead = await stream.ReadAsync(buffer, 0, buffer.Length);
+                string banner = Encoding.UTF8.GetString(buffer, 0, bytesRead);
+
+                if (!string.IsNullOrWhiteSpace(banner))
+                {
+                    Console.WriteLine($"Banner (TCP) {port}: {banner.Trim()}");
+                }
+            }
+            catch(Exception) { }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,11 +1,64 @@
 ﻿using System;
+using System.Threading.Tasks;
+using Porky.Classes;
 
 namespace Porky 
 {
     class Program
     {
-        static void Main(string[] args)
+        ///dotnet publish -c Release -r win-x64 --self-contained true -p:PublishSingleFile=true -o output
+        static async Task Main(string[] args)
         {
+            // need to set global variables 
+            string target = "";
+            int startPort = 0; 
+            int endPort = 0;
+            bool scanAllPorts = false;
+
+            // need to grab arguments to then call the port scanner
+            for (int i = 0; i < args.Length; i++)
+            {
+                switch (args[i])
+                {
+                    case "-ip":
+                        target = args[i]; break; //need to fix arguments
+                    case "-p":
+                        string[] ports = args[++i].Split('-');
+                        startPort = int.Parse(ports[0]);
+                        endPort = ports.Length > 1 ? int.Parse(ports[1]) : startPort; break;
+                    case "-all":
+                        scanAllPorts = true; break;
+                }
+            }
+           
+            if (String.IsNullOrEmpty(target))
+            {
+                Console.Write("Please Enter Ip or Hostname: ");
+                target = Console.ReadLine();
+            }
+
+            if (!scanAllPorts)
+            {
+                if (startPort == 0 || endPort == 0)
+                {
+                    Console.Write("Enter starting Port: ");
+                    startPort = int.Parse(Console.ReadLine());
+
+                    Console.Write("Enter ending Port: ");
+                    endPort = int.Parse(Console.ReadLine());
+                }
+            }
+            else
+            {
+                startPort = 1;
+                endPort = 65535;
+            }
+
+            Classes.Porky pork = new Classes.Porky();
+
+            await pork.Sizzle(target, startPort, endPort);
+            
+            // might need to create identifyservice class to handle that function. 
             Console.WriteLine("Hello, World!");
         }
     }
