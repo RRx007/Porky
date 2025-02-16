8a5987df8e6c254b54598391e3cc1deaa587a2d0
Riicky <Riicky@DESKTOP-5U9BOG6>
2025-02-16 00:36:18 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Porky 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
+
+
